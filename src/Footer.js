e752a162e3f07a7a80a058730c4cb14c525c35e7
yasmim <micayaya70@gmail.com>
2020-03-01 17:59:18 -0300

    Centralização do logo e icones

--- src/Footer.js
+++ src/Footer.js
@@ -8,56 +8,57 @@ import youtube from '../src/imagens/youtube.PNG';
 import twitter from '../src/imagens/twitter.PNG';
 import linkedin from '../src/imagens/linkedin.PNG';
 import { MdCopyright } from 'react-icons/md';
-import {TiSocialFacebook, TiSocialInstagram, TiSocialLinkedin, TiSocialTwitter, TiSocialYoutube} from 'react-icons/ti';
+import { TiSocialFacebook, TiSocialInstagram, TiSocialLinkedin, TiSocialTwitter, TiSocialYoutube } from 'react-icons/ti';
 class Footer extends Component {
     render() {
         return (
-            
-                <div className="container-fluid ">
-                    <hr />
-                    <div className="d-none d-sm-none d-md-block d-lg-block">
-                    <div className="row justify-content-center">
-                       
-                        <div className="col-md-6" >
-                            <p><img src={logo} className='logoo'/><MdCopyright /> 2020 Bico</p>
-                        </div>
-                        <div className="col-md-2">
-                            <p className='fote' ><TiSocialFacebook/> <TiSocialInstagram/> <TiSocialLinkedin/> <TiSocialTwitter/> <TiSocialYoutube/>  </p>
-                        </div>
-                    </div>
 
-                    </div>
-                    
-                    <div className="d-block d-sm-block d-md-none d-lg-none">
-                    
+            <div className="container-fluid ">
+                <hr />
+                <div className="d-none d-sm-none d-md-block d-lg-block">
+                    <div className="row">
+
+                        <div className="col-md-12">
+                            <p style={{ paddingLeft: "480px" }}><img src={logo} className='logoo' /><MdCopyright /> 2020 PomBico</p>
+                        </div>
                         <div className="row">
-                            <div className="d-block d-sm-block d-md-none col-12">
-                                <p className='fote'><MdCopyright /> 2020 Bico</p>
+                            <div className="col-md-12">
+                                 <p className='fote' style={{ paddingLeft: "580px" }}><TiSocialFacebook /> <TiSocialInstagram /> <TiSocialLinkedin /> <TiSocialTwitter /> <TiSocialYoutube />  </p>
                             </div>
                         </div>
-                        <div className="row d-block d-sm-block d-md-none d-lg-none">
-                            <div className="d-block d-sm-block d-md-none col-12">
-                                <p className='fote' style={{marginLeft:"-50px"}}><img src={logo} className=' logoo' /></p>
-                            </div>
-                        </div>
-                        <div className="row d-block d-sm-block d-md-none d-lg-none">
-                            <div className="d-block d-sm-block d-md-none col-12">
-                            <p className='fote'><TiSocialFacebook/> <TiSocialInstagram/> <TiSocialLinkedin/> <TiSocialTwitter/> <TiSocialYoutube/>  </p>
-                        </div>
-                        </div>
-
                     </div>
-                    
+                </div>
 
-                    
+                <div className="d-block d-sm-block d-md-none d-lg-none">
 
-                    
+                    <div className="row">
+                        <div className="d-block d-sm-block d-md-none col-12">
+                            <p className='fote'><MdCopyright /> 2020 PomBico</p>
+                        </div>
+                    </div>
+                    <div className="row d-block d-sm-block d-md-none d-lg-none">
+                        <div className="d-block d-sm-block d-md-none col-12">
+                            <p className='fote' style={{ marginLeft: "-50px" }}><img src={logo} className=' logoo' /></p>
+                        </div>
+                    </div>
+                    <div className="row d-block d-sm-block d-md-none d-lg-none">
+                        <div className="d-block d-sm-block d-md-none col-12">
+                            <p className='fote'><TiSocialFacebook /> <TiSocialInstagram /> <TiSocialLinkedin /> <TiSocialTwitter /> <TiSocialYoutube />  </p>
+                        </div>
+                    </div>
+
                 </div>
 
 
 
 
-        
+
+            </div>
+
+
+
+
+
         )
     }
 }
