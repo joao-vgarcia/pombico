bad3339cd1f038cbbb16922de9e22aef31060456
yasmim <micayaya70@gmail.com>
2020-03-02 21:37:18 -0300

    modificando footer novamente

--- src/Footer.js
+++ src/Footer.js
@@ -4,42 +4,24 @@ import logo from '../src/imagens/logoPombo.PNG';
 import './Footer.css';
 
 import { MdCopyright } from 'react-icons/md';
-import {TiSocialFacebook, TiSocialInstagram, TiSocialLinkedin, TiSocialTwitter, TiSocialYoutube} from 'react-icons/ti';
+import { TiSocialFacebook, TiSocialInstagram, TiSocialLinkedin, TiSocialTwitter, TiSocialYoutube } from 'react-icons/ti';
 class Footer extends Component {
     render() {
         return (
-            <div>
-            
-                <div className="container-fluid ">
-                    <hr />
-                    <div className="d-none d-sm-none d-md-block d-lg-block">
-                    <div className="row justify-content-center">
-                       
-                        <div className="col-md-6" >
-                            <p><Link to="/home" ><img src={logo} className='logoo' alt="Logo"/></Link><MdCopyright /> 2020 Bico</p>
-                        </div>
-                        <div className="row ">
-                            <div className="col-md-12">
-                                <p className='fote' > <a href="https://www.facebook.com/fazUmBico/" target="blank"><TiSocialFacebook/></a> <a href="https://www.instagram.com/pombico/" target="blank"><TiSocialInstagram/></a> <TiSocialLinkedin/> <TiSocialTwitter/> <TiSocialYoutube/>  </p>
-                            </div>
-                            <div className="col-md-12">
-                                <button className="btn btn-block" style={{backgroundColor:"#32329C", borderRadius:"35px", color:"white"}}>Baixe nosso app</button>
-                            </div>
-                        </div>
-                        
-                    </div>
 
+            <div className="container-fluid ">
+                <hr />
+                <div className="d-none d-sm-none d-md-block d-lg-block">
+                    <div className="row">
 
-                    </div>
-                    
-                    <div className="d-block d-sm-block d-md-none d-lg-none">
-                    
+                        <div className="col-md-12">
+                            <p style={{ paddingLeft: "480px" }}><img src={logo} className='logoo' /><MdCopyright /> 2020 PomBico</p>
+                        </div>
                         <div className="row">
-                            <div className="d-block d-sm-block d-md-none col-12">
-                                <p className='fote'><MdCopyright /> 2020 Bico</p>
+                            <div className="col-md-12">
+                                <p className='fote' style={{ paddingLeft: "580px" }}><a href="https://www.facebook.com/fazUmBico/" target="blank"><TiSocialFacebook /></a> <a href="https://www.instagram.com/pombico/" target="blank"><TiSocialInstagram /></a> <TiSocialLinkedin /> <TiSocialTwitter /> <TiSocialYoutube /></p>
                             </div>
                         </div>
-
                     </div>
                 </div>
 
@@ -57,43 +39,26 @@ class Footer extends Component {
                     </div>
                     <div className="row d-block d-sm-block d-md-none d-lg-none">
                         <div className="d-block d-sm-block d-md-none col-12">
-                            <p className='fote'><TiSocialFacebook /> <TiSocialInstagram /> <TiSocialLinkedin /> <TiSocialTwitter /> <TiSocialYoutube />  </p>
-
-                        <div className="row d-block d-sm-block d-md-none d-lg-none">
-                            <div className="d-block d-sm-block d-md-none col-12">
-                                <p className='fote'><Link to="/home"  style={{marginLeft:"-50px"}}><img src={logo} className='logoo' alt="Logo"/></Link></p>
-                            </div>
+                            <p className='fote'><a href="https://www.facebook.com/fazUmBico/" target="blank"><TiSocialFacebook /></a> <a href="https://www.instagram.com/pombico/" target="blank"><TiSocialInstagram /></a> <TiSocialLinkedin /> <TiSocialTwitter /> <TiSocialYoutube /></p>
                         </div>
-                        <div className="row d-block d-sm-block d-md-none d-lg-none">
-                            
-                            <div className="d-block d-sm-block d-md-none col-12">
-                             <p className='fote' > <a href="https://www.facebook.com/fazUmBico/" target="blank"><TiSocialFacebook/></a> <a href="https://www.instagram.com/pombico/" target="blank"><TiSocialInstagram/></a> <TiSocialLinkedin/> <TiSocialTwitter/> <TiSocialYoutube/>  </p>
-                            </div>
-
-                        </div>
-                        <div className="row d-block d-sm-block d-md-none d-lg-none">
-                            
-                            <div className="col-md-12">
-                                <button className="btn btn-block" style={{backgroundColor:"#32329C", borderRadius:"35px", color:"white"}}>Baixe nosso app</button>
-                            </div>
-
-                        </div>
-
                     </div>
-                    
 
-                    
-
-                    
-                </div>
-                </div>
                 </div>
 
 
 
-        
+
+
+            </div>
+
+
+
+
+
         )
     }
 }
 
 export default Footer;
+
+
--- src/paginas/home.js
+++ src/paginas/home.js
@@ -36,7 +36,7 @@ class Home extends Component { //criando a classe
     render() {
         return (
             <div> {/**Div que recebe todas as divs */}
-                
+
                 {/**Primeiro container */}
                 <div className="container-fluid justify-content-center" >
                     {/**primeira linha com os textos */}
@@ -49,7 +49,7 @@ class Home extends Component { //criando a classe
                              */}
                             <div className="d-none d-md-block"><br /><br /><br /></div>
 
-                      
+
 
                             {/**Quebra de linha de acordo com o tamanho da tela */}
                             <div className="d-none d-md-block"><br /></div><div className="d-block d-sm-block d-md-none"><br /></div>
@@ -77,10 +77,10 @@ class Home extends Component { //criando a classe
 
                         {/**Coluna da direita com imagem dentro da linha 1 */}
                         <div className="col-md-12 col-lg-5 ">
-                        
-                            <div className="d-none d-sm-none d-md-block"><lottie-player src="https://assets2.lottiefiles.com/packages/lf20_F2Mv1p.json" speed="0.75"  style={ {width: "500px", height: "500px",background:"transparent"}} className="img-fluid"  loop autoplay > </lottie-player></div>
-                            <div className="d-block d-sm-block d-md-none d-lg-none"><lottie-player src="https://assets2.lottiefiles.com/packages/lf20_F2Mv1p.json" speed="1"  style={ {width: "200px", height: "200px",background:"transparent", marginLeft:"70px"}} className="img-fluid"  loop autoplay > </lottie-player></div>
-                            
+
+                            <div className="d-none d-sm-none d-md-block"><lottie-player src="https://assets2.lottiefiles.com/packages/lf20_F2Mv1p.json" speed="0.75" style={{ width: "500px", height: "500px", background: "transparent" }} className="img-fluid" loop autoplay > </lottie-player></div>
+                            <div className="d-block d-sm-block d-md-none d-lg-none"><lottie-player src="https://assets2.lottiefiles.com/packages/lf20_F2Mv1p.json" speed="1" style={{ width: "200px", height: "200px", background: "transparent", marginLeft: "70px" }} className="img-fluid" loop autoplay > </lottie-player></div>
+
                         </div>
                     </div>
 
@@ -99,17 +99,17 @@ class Home extends Component { //criando a classe
 
                         <div className="col-12">
                             {/**texto centralizado */}
-                            <h1 className="text-center funciona">Como funciona</h1>
+                            <h1 className="text-center funciona"><b>Como funciona</b></h1>
                         </div>
                     </div>
                 </div>
                 {/**Fim do container */}
-                   {/**Inicio do carousel */}
-                   <div className="row mx-auto" >
+                {/**Inicio do carousel */}
+                <div className="row mx-auto" >
                     <div className="col-12">
-                        <div id="comoFunciona" className="carousel slide" data-ride="carousel" style={{height:"430px"}}>
+                        <div id="comoFunciona" className="carousel slide" data-ride="carousel" data-interval="0" style={{ height: "430px" }}>
 
-                                                                        
+
                             <ul className="carousel-indicators">
                                 <li data-target="#comoFunciona" data-slide-to="0" className="active"></li>
                                 <li data-target="#comoFunciona" data-slide-to="1"></li>
@@ -117,62 +117,53 @@ class Home extends Component { //criando a classe
                             </ul>
 
 
-                            <div className	="carousel-inner">
-                                <div className	="carousel-item active">
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
                                     <div className="row justify-content-center">
-                                        <div className="col-sm-6 col-md-5 ">
-                                            <p className="num">1</p>
-                                            <p className="titulo">Cadastre-se</p>
-                                            <p >Seja você prestador ou cliente, é preciso realizar um rápido cadastro para a segurança de todos os usuários</p>
+                                        <div className="col-sm-6 col-md-6 ">
+                                            <h2 className="num" style={{ paddingLeft: "140px" }}><b>1-</b></h2>
+                                            <h3 className="titulo" style={{ paddingLeft: "140px" }}><b>Cadastre-se</b></h3>
+                                            <h4 style={{ paddingLeft: "140px" }}>Seja você prestador ou cliente, é preciso realizar um rápido cadastro para a segurança de todos os usuários</h4>
                                         </div>
-                                        <div className="col-sm-6 col-md-5">
-                                        <lottie-player src={"https://assets6.lottiefiles.com/packages/lf20_iXKIMU.json"}  speed={1}  style={{width: "200px", height: "200px",background:"transparent"}}  loop autoplay ></lottie-player>
-
-                                        </div>
+                                        <div className="col-sm-6 col-md-5" style={{ paddingLeft: "160px", paddingTop: "35px" }}><img src={register} className="img-fluid" /></div>
                                     </div>
-                                   
+
                                 </div>
-                                <div className	="carousel-item">
+                                <div className="carousel-item">
                                     <div className="row justify-content-center">
                                         <div className="col-sm-6 col-md-5 ">
-                                            <p className=" num">2</p>
-                                            <p className=" titulo">Busque por um serviço</p>
-                                            <p className="">Navegue entre as diversas categorias de serviços no app ou procure por algo específico</p>
+                                            <h2 className="num" style={{ paddingLeft: "140px" }}><b>2-</b></h2>
+                                            <h3 className="titulo" style={{ paddingLeft: "140px" }}><b>Busque por um serviço</b></h3>
+                                            <h4 className="" style={{ paddingLeft: "140px" }}>Navegue entre as diversas categorias de serviços no app ou procure por algo específico</h4>
                                         </div>
-                                        <div className="col-sm-6 col-md-5">
-                                        <lottie-player src={"https://assets4.lottiefiles.com/datafiles/qSKave3osZaw1QT/data.json"}  speed={1.3}  style={{width: "200px", height: "200px",background:"transparent"}}  loop autoplay ></lottie-player>
+                                        <div className="col-sm-6 col-md-5" style={{ paddingLeft: "160px", paddingTop: "10px" }}>
+                                            <lottie-player src={"https://assets4.lottiefiles.com/datafiles/qSKave3osZaw1QT/data.json"} speed={1.3} style={{ width: "180px", height: "180px", background: "transparent" }} loop autoplay ></lottie-player>
 
                                         </div>
                                     </div>
                                 </div>
-                                <div className	="carousel-item">
-                                    <div className="row justify-content-center"> 
+                                <div className="carousel-item">
+                                    <div className="row justify-content-center">
 
-                                    
+
                                         <div className="col-sm-6 col-md-5 ">
-                                            <p className=" num">3</p>
-                                            <p className=" titulo">Negocie com um prestador</p>
-                                            <p className="">Conecte-se com prestadores pertos de você negocie o serviço direto pelo app</p>
+                                            <h2 className="num" style={{ paddingLeft: "140px" }}><b>3-</b></h2>
+                                            <h3 className="titulo" style={{ paddingLeft: "140px" }}><b>Negocie com um prestador</b></h3>
+                                            <h4 className="" style={{ paddingLeft: "140px" }}>Conecte-se com prestadores pertos de você negocie o serviço direto pelo app</h4>
                                         </div>
-                                        <div className="col-sm-6 col-md-5">
-                                        <lottie-player src={"https://assets6.lottiefiles.com/packages/lf20_ycNQ7E.json"}  speed={1.4}  style={{width: "200px", height: "200px",background:"transparent"}}  loop autoplay ></lottie-player>
-
-                                        </div>
+                                        <div className="col-sm-6 col-md-5" style={{ paddingLeft: "160px", paddingTop: "35px" }}><img src={maos} className="img-fluid" /></div>
 
                                     </div>
                                 </div>
                                 <div className="carousel-item">
                                     <div className="row justify-content-center">
-                                                
-                                        <div className="col-sm-6 col-md-5 ">
-                                            <p className=" num">4</p>
-                                            <p className=" titulo">Tudo pronto</p>
-                                            <p className="">Depois de negociar você poderá marcar date e horário para o serviço acontecer e realizar o pagamento pelo app</p>
-                                        </div>
-                                        <div className="col-sm-6 col-md-5">
-                                        <lottie-player src={"https://assets4.lottiefiles.com/packages/lf20_5zYhWw.json"}  speed={1}  style={{width: "200px", height: "200px",background:"transparent"}}  loop autoplay ></lottie-player>
 
+                                        <div className="col-sm-6 col-md-5 ">
+                                            <h2 className="num" style={{ paddingLeft: "140px" }}><b>4-</b></h2>
+                                            <h3 className="titulo" style={{ paddingLeft: "140px" }}><b>Tudo pronto</b></h3>
+                                            <h4 className="" style={{ paddingLeft: "140px" }}>Depois de negociar você poderá marcar data e horário para o serviço acontecer e realizar o pagamento pelo app</h4>
                                         </div>
+                                        <div className="col-sm-6 col-md-5" style={{ paddingLeft: "160px", paddingTop: "35px" }}><img src={beleza} className="img-fluid" /></div>
 
                                     </div>
 
@@ -182,61 +173,61 @@ class Home extends Component { //criando a classe
 
 
                             <a className="carousel-control-prev" href="#comoFunciona" data-slide="prev" >
-                            <span><IoIosArrowRoundBack style={{backgroundColor:"#30305d", fontSize:"56px"}}/></span>
+                                <span><IoIosArrowRoundBack style={{ backgroundColor: "#30305d", fontSize: "56px" }} /></span>
                             </a>
                             <a className="carousel-control-next" href="#comoFunciona" data-slide="next" >
-                            <span ><IoIosArrowRoundForward style={{backgroundColor:"#30305d", fontSize:"56px"}}/></span>
+                                <span ><IoIosArrowRoundForward style={{ backgroundColor: "#30305d", fontSize: "56px" }} /></span>
                             </a>
                             {/**className="carousel-control-prev-icon" */}
-                             {/**className="carousel-control-next-icon" */}
+                            {/**className="carousel-control-next-icon" */}
                         </div>
 
                     </div>
 
 
                 </div>
-                
+
                 {/**Fim do carousel */}
 
 
 
-                
+
 
                 {/**container de 2 linha */}
                 <div className="container-fluid">
                     {/**linha azul */}
-                    <div className="row seguro align-items-center">
+                    <div className="row seguro align-items-center" style={{ padding: "10px" }}>
                         {/**colunas da esquerda com quebra de linha de acorda com a tela */}
-                        <div className="col-sm-12 col-md-7" >
+                        <div className="col-sm-12 col-md-6" >
 
-                            <h2 className="seguro" id="titulofac"> <br/>Seguro e confiável</h2>
+                            <h2 className="seguro" id="titulofac"> <br /><b>Seguro e confiável</b></h2>
                             <h3 className="textoseguro">Todo o processo de negociação é feito pelo app, o que garante a segurança dos usuários contra possíveis fraudes</h3>
                             <h3 className="textoseguro">Além disso, prestadores e clientes são avaliados uns pelos outros, aumentando a confiabilidade nas negociações!</h3>
-                            <div><br/></div>
+                            <div><br /></div>
                         </div>
                         {/**coluna da direita com imagem */}
-                        <div className="col-md-5">
+                        <div className="col-md-6" style={{ paddingLeft: "220px" }}>
                             {/**imagem que some em telas menores */}
-                            <div className="d-none d-md-block"><img src={homem} className="img-fluid" style={{marginLeft:"20px"}}/></div>
+                            <div className="d-none d-md-block"><img src={shield} className="img-fluid" style={{ marginLeft: "20px" }} /></div>
 
                         </div>
                     </div>
 
                     {/**linha verde */}
-                    <div className="row rapido align-items-center" >
+                    <div className="row rapido align-items-center" style={{ padding: "35px" }}>
 
                         {/**coluna esquerda com imagem  */}
-                        <div className="col-md-5">
-                            <div className="d-none d-md-block"><img src={mulher} className="img-fluid" style={{marginLeft:"-12px"}} /></div>
+                        <div className="col-md-5" style={{ paddingLeft: "120px" }}>
+                            <div className="d-none d-md-block"><img src={run} className="img-fluid" style={{ marginLeft: "-12px" }} /></div>
 
                         </div>
                         {/**coluna da esquerda com linhas responsivas */}
                         <div className="col-sm-12 col-md-7 rapido" >
 
-                            <h2 id="titulofac"><br/>Rápido e fácil de usar</h2>
+                            <h2 id="titulofac"><br /><b>Rápido e fácil de usar</b></h2>
                             <h3 className="textorapido">Com poucas ações você consegue buscar e contratar prestadores de serviços na sua região</h3>
                             <h3 className="textorapido">Também é possível filtrar profissionais por distância, serviços oferecidos e mais!</h3>
-                            <div><br/></div>
+                            <div><br /></div>
 
                         </div>
                     </div>{/**fim da linha verde */}
@@ -249,27 +240,23 @@ class Home extends Component { //criando a classe
 
                     {/**linha unica */}
                     <div className="row justify-content-center">
-                        {/**coluna da esquerda com uma imagem que some em telas pequenas */}
-                        <div className="col-md-6">
-                            <div className="d-none d-md-block"><lottie-player src={"https://assets4.lottiefiles.com/temp/lf20_3tPLQ7.json"} speed={1}  style={{width: "500px", height: "300px", background:"transparent"}}  loop autoplay ></lottie-player></div>
 
-                        </div>
                         {/**colunas da direita com linhas responsivas */}
 
-                        <div className="col-sm-12 col-md-6">
+                        <div className="col-sm-12 col-md-12">
                             <div><br /></div>
 
                             <br />{/**quebra de linha fixa */}
 
-                            <h1 id="baixar"><center>Baixe nosso aplicativo</center></h1>
-                            <hr />{/**quebra de linha com um traço */}
-                            <h2 className="textobaixar">Faça o Download do app e contrate os serviços que precisa, sem complicações</h2>
-                            <h2 className="textobaixar">Disponível para aparelhos móveis</h2>
-                            {/**uma linha dentro de uma linha com duas imagens fixas */}
-                             
-                                <button className="btn btnai btn-block" > <b>Baixar</b></button>
-                             
+                            <h2 id="baixar"  style={{textAlign: 'center'}}><b>Baixe nosso aplicativo</b></h2>
+                            <h3 className="textobaixar" style={{textAlign: 'center'}}>Faça o Download do app e contrate os serviços que precisar, sem complicações</h3>
+                         {/**<button className="btn btnai btn-block" > <b>Baixar</b></button>*/}
+
                         </div>
+
+                    </div>
+                    <div className="col-md-12" style={{ paddingLeft:"560px"}}>
+                        <div className="d-none d-md-block" style={{ padding: "10px"}}><img src={baixar} className="img-fluid"/></div>
 
                     </div>
 
