3f3beea69c173b0d2eb9ade1b53667d792f9c63a
yasmim <micayaya70@gmail.com>
2020-03-05 20:09:04 -0300

    comentando um link nao usado

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import logo from '../src/imagens/logoPombo.PNG';
 import './Footer.css';
 
